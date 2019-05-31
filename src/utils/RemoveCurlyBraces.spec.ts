12a44d6c1ad940f8f4d648fa764c50c9927f7f9a
rolly <rollrodrig@gmail.com>
2019-05-31 17:05:18 -0500

    more test done

--- src/utils/RemoveCurlyBraces.spec.ts
+++ src/utils/RemoveCurlyBraces.spec.ts
@@ -2,6 +2,8 @@ import { expect, assert } from 'chai';
 import {removeCurlyBraces} from './RemoveCurlyBraces';
 describe('removeCurlyBraces: ', () => {
 	it('should remove curly braces {} if it exist', () => {
+        expect(removeCurlyBraces("{name:string}")).equal("name:string");
+        expect(removeCurlyBraces("{name:string, email:string}")).equal("name:string, email:string");
         expect(removeCurlyBraces("{hello}")).equal("hello");
         expect(removeCurlyBraces("{hello")).equal("hello");
         expect(removeCurlyBraces("hello}")).equal("hello");
@@ -11,5 +13,8 @@ describe('removeCurlyBraces: ', () => {
         expect(removeCurlyBraces("he{ll}o")).equal("he{ll}o");
         expect(removeCurlyBraces("he{}o")).equal("he{}o");
         expect(removeCurlyBraces("he{ }o")).equal("he{ }o");
+        expect(removeCurlyBraces("[he{ }o]")).equal("[he{ }o]");
+        expect(removeCurlyBraces("{[123]}")).equal("[123]");
+        expect(removeCurlyBraces("[[he{ }o]]")).equal("[[he{ }o]]");
     });
 });
--- src/utils/RemoveSqueareBrackets.spec.ts
+++ src/utils/RemoveSqueareBrackets.spec.ts
@@ -1,8 +1,11 @@
 import { expect, assert } from 'chai';
 import { removeSquareBrackets } from './RemoveSquareBrackets';
 describe('removeSquareBrackets: ', () => {
-	it('should remove curly braces [] if it exist', () => {
-        expect(removeSquareBrackets("[hello]")).equal("hello");
+    it('should remove curly braces [] if it exist', () => {
+        expect(removeSquareBrackets("[{name:string}]")).equal("{name:string}");
+        expect(removeSquareBrackets("[{name:string},10]")).equal("{name:string},10");
+        expect(removeSquareBrackets("[{name:string, phone:number},10]")).equal("{name:string, phone:number},10");
+        expect(removeSquareBrackets("[{hello}]")).equal("{hello}");
         expect(removeSquareBrackets("[hello")).equal("hello");
         expect(removeSquareBrackets("hello]")).equal("hello");
         expect(removeSquareBrackets("hello")).equal("hello");
