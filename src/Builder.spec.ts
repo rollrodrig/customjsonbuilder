2c638d90a2fcf45119540802ae012358c3014c1d
rolly <rollrodrig@gmail.com>
2019-06-04 18:32:18 -0500

    adding syntax validator

--- src/Builder.spec.ts
+++ src/Builder.spec.ts
@@ -3,18 +3,27 @@ import Builder from './Builder';
 import faker from 'faker';
 
 describe('Builder: ', () => {
-	it('.getJson: should return the json ', () => {
+	it('.getResponse: should return the json ', () => {
         let builder = new Builder();
         builder.setPattern("{name:string}");
         let response = builder.getResponse();
         expect(response).to.deep.eq({name:response.name})
     });
+    it('.validatePattern: should not throw error ', () => {
+        let b1 = new Builder("{name:string,data:{a:string,b:number}}");
+        expect(()=>{b1.validatePattern()}).to.not.throw(SyntaxError);
+
+        let b2 = new Builder("{name:string,data:[{a:string,b:number};10],age:number}");
+        expect(()=>{b2.validatePattern()}).to.not.throw(SyntaxError);
+    });
+    it('.validatePattern: pattern is wrong should not throw error ', () => {
+        let b1 = new Builder("{name:string");
+        expect(()=>{b1.validatePattern()}).to.throw(SyntaxError);
+
+        let b2 = new Builder("{name:string,name:{a:string,age:number}");
+        expect(()=>{b2.validatePattern()}).to.throw(SyntaxError);
+
+        let b3 = new Builder("{name:string,name:[{a:string};10,age:number}");
+        expect(()=>{b3.validatePattern()}).to.throw(SyntaxError);
+    });
 });
-
-
-// // let builder = new Builder("{name:string}");
-        // // assert.isNotNull(builder.getJson());
-        // let s1 = "name:string,other,onemore,other";
-        // let s2 = "name:string,word:{class:other},onemore,other";
-        // let g1 = s2.match(/([a-z:{}]+)/g);
-        // console.log(g1);
--- src/Builder.ts
+++ src/Builder.ts
@@ -1,6 +1,9 @@
 import faker from 'faker';
 import { build } from './constructors/build';
 import { generateResponse } from './constructors/generateResponse';
+import { spaceCleaner } from './singleTask/SpaceCleaner';
+import { isBracesEquals } from './validator/isBracesEquals';
+import { isSquareEquals } from './validator/isSquareEquals';
 class Builder {
     pattern:string;
     response:any;
@@ -10,7 +13,17 @@ class Builder {
     setPattern(p:string) {
         this.pattern = p;
     }
+    validatePattern() {
+        this.pattern = spaceCleaner(this.pattern);
+        if(isBracesEquals(this.pattern) === false) {
+            throw SyntaxError("Syntax error, make sure open and close {} are in the pattern")
+        }
+        if(isSquareEquals(this.pattern) === false) {
+            throw SyntaxError("Syntax error, make sure open and close [] are in the pattern")
+        }
+    }
     getResponse() {
+        this.validatePattern()
         this.response = generateResponse(build(this.pattern))
         return this.response;
     }
