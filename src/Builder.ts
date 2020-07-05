565a6273933c5e7070025beffc878960e2501acc
rolly <rollrodrig@gmail.com>
2020-07-05 13:06:03 -0500

    everything is working

--- src/Builder.ts
+++ src/Builder.ts
@@ -2,14 +2,17 @@ import Cleaner from "./cleaner/cleaner";
 import Validator from "./validator/validator";
 import { Generator } from "./generator/generator";
 import Error from "./error/error";
+import { Reader } from "./reader/reader";
 
 class Builder {
 	cleaner: Cleaner;
 	validator: Validator;
+	reader: Reader;
 	generator: Generator;
 	constructor() {
 		this.cleaner = new Cleaner();
 		this.validator = new Validator();
+		// this.reader = new Reader();
 	}
 	run(pattern: string): any {
 		pattern = this.cleaner.run(pattern);
